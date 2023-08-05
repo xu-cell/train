672641c5cb55f668cb6dc0b690032812284df9d8
xu-cell <2459811340@qq.com>
2023-08-05 13:10:45 +0800

    会员模块搭建

---
+++ member/src/main/java/com/xujiajun/train/member/config/MemberApplication.java
@@ -0,0 +1,17 @@
+package com.xujiajun.train.member.config;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author xujj
+ */
+@SpringBootApplication
+@ComponentScan("com.xujiajun")
+public class MemberApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class, args);
+    }
+}
--- member/src/main/java/com/xujiajun/train/member/controller/TestController.java
+++ member/src/main/java/com/xujiajun/train/member/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.xujiajun.train;
+package com.xujiajun.train.member.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,10 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author xujj
  */
+
 @RestController
 public class TestController {
     @GetMapping("/hello")
     public String hello() {
-        return "hello world aa world";
+        return "Hello!!!!!";
     }
 }
