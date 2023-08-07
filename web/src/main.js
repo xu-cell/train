448875a5419b69ec16b56c17ab4d900c9b31cd40
xu-cell <2459811340@qq.com>
2023-08-07 09:39:29 +0800

    集成Ant Design Vue 及其图标

--- web/src/main.js
+++ web/src/main.js
@@ -1,6 +1,15 @@
 import { createApp } from 'vue'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as Icons from '@ant-design/icons-vue'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+app.use(Antd).use(store).use(router).mount('#app');
+
+const icons = Icons;
+for(const icon in icons) {
+    app.component(icon, icons[icon]);
+}
